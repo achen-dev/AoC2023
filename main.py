d1a94c78d2f7daaef550e2672dd5a1f91db39c4c
achen-dev <chenhaojunchj@gmail.com>
2023-12-04 00:01:44 +1100

    Day 1 + Start of Day2

---
+++ main.py
@@ -0,0 +1,64 @@
+# This is the driver code for the 2023 AoC event
+
+def day_one(input_file):
+    with open(input_file) as infile:
+        lines_list = infile.read().splitlines()
+    # print(lines_list)
+
+    # Part 1:
+    part1_total_sum = 0
+
+    for line in lines_list:
+        line_digits = []
+        for char in line:
+            if char.isnumeric():
+                line_digits.append(char)
+        # print(line_digits)
+        if line_digits:
+            line_sum = int(line_digits[0] + line_digits[-1])
+            part1_total_sum += line_sum
+    print("Part 1 Total Sum:", part1_total_sum)
+
+    # Part 2:
+    part2_total_sum = 0
+    char_dig_dict = {"one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9}
+
+    for line in lines_list:
+        line_digits = []
+        char_accumulator = ""
+        for char in line:
+            if char.isnumeric():
+                line_digits.append(char)
+            else:
+                char_accumulator += char
+                for char_key in char_dig_dict:
+                    if char_key in char_accumulator:
+                        line_digits.append(str(char_dig_dict[char_key]))  # Hacky solution to avoid rewriting dict
+                        char_accumulator = char
+                    else:
+                        continue
+
+        line_sum = int(line_digits[0] + line_digits[-1])
+        part2_total_sum += line_sum
+    print("Part 2 Total Sum:", part2_total_sum)
+
+
+def day_two(input_file):
+    with open(input_file) as infile:
+        lines_list = infile.read().splitlines()
+    print(lines_list)
+
+    # Part 1
+    RED_LIMIT = 12
+    GREEN_LIMIT = 13
+    BLUE_LIMIT = 14
+
+    for line in lines_list:
+        game_id = line.split(" ")[1][:-1]
+        print(game_id)
+
+
+if __name__ == '__main__':
+    # print("Day 1:")
+    # day_one('inputs/day1')
+    day_two("inputs/day2")
